cb6accfedad7f1fe71aa5f9d12e32eb50e18be31
arun.kanagaraj@ebix.com <arun.kanagaraj@ebix.com>
2018-04-16 14:23:50 +0530

    project init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  fname = '';
+  lname = '';
+  email = '';
+  pwd = '';
+  cpwd = '';
+
+  onRegister() {
+    alert(this.fname + this.lname + this.email);
+  }
+  onSubmit(f: NgForm) {
+    console.log(f.form.value);  // { first: '', last: '' }
+    // console.log(f.valid);  // false
+  }
 }
